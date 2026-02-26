f67a9c7778be51781bea6728efdb9aafcc30b1f0
Thomas Flori <thflori@gmail.com>
2026-02-26 10:48:46 +0100

    undefine how a Kernel works
    To be able to define required parameters to handle we had to remove it. And if you want to store the Application container, use `app()` to get one or pass dependencies to the constructors is up to the implementor.

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,25 +5,17 @@ namespace Riki;
 /**
  * Class Kernel
  *
+ * A Kernel is the main entry point to handle a request, a command or whatever type of "task"
+ * you define your application can handle.
+ *
+ * It requires a method handle. Typically this method requires some sort of "input" and returns
+ * "output" - for a CLI kernel that might be `argv` and an integer; for an HTTP kernel a
+ * Request and Response object.
+ *
  * @package Riki
  * @author  Thomas Flori <thflori@gmail.com>
+ * @method mixed handle(...$args)
  */
 abstract class Kernel
 {
-    /**
-     * Kernel constructor.
-     *
-     * @param Application $app
-     * @codeCoverageIgnore
-     */
-    public function __construct(Application $app)
-    {
-        $this->app = $app;
-    }
-
-    /**
-     * Handle the execution
-     * @return mixed
-     */
-    abstract public function handle();
 }
